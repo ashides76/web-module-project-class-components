a831b3c0ab4d550e2acee1662687ec62d97a79f8
Ashish Desai <desai.ashish76@gmail.com>
2023-07-15 22:22:48 -0400

    build from state and completed flag

--- frontend/components/App.js
+++ frontend/components/App.js
@@ -1,21 +1,48 @@
 import React from 'react'
+import { v4 as uuid } from 'uuid';
 
 export default class App extends React.Component {
+  constructor() {
+    super()
+    this.state = {
+      todos: [
+        {
+          name: 'Organize Garage',
+          id: uuid(), // could look different, you could use a timestamp to generate it
+          completed: false
+        },
+        {
+          name: 'Bake Cookies',
+          id: uuid(),
+          completed: false
+        },
+        {
+          name: 'Walk the dog',
+          id: uuid(),
+          completed: true
+        },
+      ]
+    }
+  }
   render() {
+    const { todos } = this.state;
+    console.log(todos);
+
     return (
       <div>
         <h1>Todo App</h1>
         <ul>
-          <li>Make dinner</li>
-          <li>Walk the dog</li>
-          <li>Brush the teeth</li>
+          {
+            todos.map((todo) => {
+              return (<li key={todo.id}>{todo.name} {todo.completed ? <span> - completed </span> : <span></span>}</li>)
+            })
+          }
+        </ul>
         <form>
-          <input></input>
+          <input/>
           <button>Add</button>
           <button>Clear</button>
         </form>
-
-        </ul>
       </div>
     )
   }
